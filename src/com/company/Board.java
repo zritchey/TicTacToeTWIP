a6d769acd08393ccccd358cb51e875912f78630b
zritchey <zachrritchey@gmail.com>
2016-12-23 09:55:44 -0500

    hi

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
                                     ///ArrayList of Arraylists?
@@ -40,9 +41,23 @@ public class Board {
     public char [][] returnp(){
         return play;
     }
+
+
+
     public void computer(int r ,char[][]bored){
         int[]final_spot={-1,-1};
-         int [][]a=new int [size*size][2];
+
+
+
+
+        System.out.println("The computer chose ("+(final_spot[0]+1)+", "+(final_spot[1]+1)+")" );
+    }
+
+
+
+    public void possible(char[][]brd,int p){
+
+        int [][]a=new int [size*size][2];
         int count=0;
         for (int j=0;j<size;j++){
             for(int i=0;i<size;i++){
@@ -53,91 +68,24 @@ public class Board {
                 }
             }
         }
-        int [][]free=new int[count][];
-        System.arraycopy(a,0,free,0,count);
-       int s=0;
-        int [][]choice=new int[count][4];// multidimensional array is used to store (row, column,forward,back)
-        for (int p=0;p<free.length;p++) {
-            int domain= (size-1)-free[p][0];
-            int range=(size-1)-free[p][1];
-            int back=0;
-            int forward=0;
-            int row=0;
-            int column=0;
-            for (int y = 0; y < size-1; y++) {
-                for (int x = 0; x < size-1; x++) {
-                    if (play[free[p][0]+(x-free[p][0])][free[p][1]]==Main.name[1]){
-                        row++;
-                    }
-                    if(play[free[p][0]][free[p][1]+(y-free[p][1])]==Main.name[1]){
-                        column++;
-                    }
-                    if (play[free[p][0]+(x-free[p][0])][free[p][1]+(y-free[p][1])]==Main.name[1]){
-                        forward++;
-                    }
-                    if(x<domain&&y<range) {
-                        if (play[free[p][ 0] - (x -domain)][free[p][1] - (y - range)] == Main.name[1]) {
-                            back++;     // this is all very problematic
-                        }
-                    }
-                }
-            }
-            choice [p][0]=row;
-            choice[p][1]=column;
-            choice[p][2]=forward;
-            choice[p][3]=back;
+
+        ArrayList aryl=new ArrayList();
+
+       char [][]free=new char[size][size];
+        for(int i=0;i<a.length;i++) {
+
+            System.arraycopy(brd, 0, free, 0, count);
+            free[a[i][0]][a[i][1]]=Main.name[p];
+            p=(p+1)%2;
+            if(i>1)
+            possible(free,p);
+            aryl.add(free);
         }
-        int num[]=new int[choice.length];
-        for(int p=0;p<choice.length;p++){
-            num[p]=Math.max(Math.max(choice[p][0],choice[p][1]),Math.max(choice[p][2],choice[p][3]));
-        }
-        int pick1 = Arrays.binarySearch(num, size-1);
-        for(s=0;s<num.length;s++){
-            num[s]=choice[s][0]+choice[s][1]+choice[s][2]+choice[s][3];
-        }
-        a=new int[5][2];
-        a[0][0]=size/2;
-        a[0][1]=size/2;
-        s=1;
-        for (int j=0;j<size;j+=(size-1)){
-            for(int i=0;i<size;i+=(size-1)){
-                a[s][0]=i;
-                a[s][1]=j;
-                s++;
-            }
-        }
-        if(r<=2){
-            while(1==1){
-                int ran=new Random().nextInt(a.length);
-                int find=Match(free,a[ran]);
-                if(find>-1){
-                    update(a[ran],Main.name[1],bored);
-                    final_spot=free[find];
-                    break;
-                }
-            }
-        }
-        else if (pick1>-1){
-            update(free[pick1],Main.name[1],bored);
-            final_spot=free[pick1];
-        }
-        else{
-            int indx=0;
-            for(s=0;s<num.length;s++){
-                indx=Math.max(indx,num[s]);
-            }
-            int p=-1;
-            for(int j=0;j<num.length;j++){
-                if (indx==num[j]){
-                    p=j;
-                    j=num.length;
-                }
-            }
-            update(free[p],Main.name[1],bored);
-            final_spot=free[p];
-        }
-        System.out.println("The computer chose ("+(final_spot[0]+1)+", "+(final_spot[1]+1)+")" );
+        aryl.trimToSize();
+        char[][][]poss=aryl.
     }
+
+
     public static boolean evaluate(char player){
         int [][]a=new int [size*size][2];
         int count=0;
@@ -194,6 +142,8 @@ public class Board {
         }
         return full==size;
     }
+
+
     public int Match(int[][]s,int[]f){
         int here=-1;
         for (int i=s.length-1 ;i>=0;i--){
